7a2dee3a56de679620a140d9fb110008f52613d3
Nero Tran <tranhuq@miamioh.edu>
2021-09-19 19:27:45 -0400

    continue

--- eval-poly.scm
+++ eval-poly.scm
@@ -14,8 +14,8 @@
 (define (eval-polynomial coefficients x)
   (cond
     ((null? coefficients) 0)
-    ((null? (cdr coefficients)) (car coefficients))
-    (else (+ (car coefficients) ((eval-polynomial (map (lambda (n) (* n x)) (cdr coefficients)) x)))) 
+    ((null? (cdr coefficients)) 1)
+    (else (+ (car cficieoefnts) ((eval-polynomial (map (lambda (n) (* n x)) (cdr coefficients)) x)))) 
   )
 )
 
